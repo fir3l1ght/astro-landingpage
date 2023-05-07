ec3895ee266a437f17bf897e192908f927d3bf49
fir3l1ght <tecnojpl11.study@gmail.com>
2023-05-07 20:02:45 +0200

    Page Ready

---
+++ src/components/landing/MysteryButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+let show = ref(true);
+const props = defineProps(['text']);
+
+function hideButton() {
+  show.value = false;
+}
+</script>
+
+<template>
+  <button class="bg-orange-500 rounded-xl px-3 py-2 mt-4 hover:bg-indigo-400" @click="hideButton" v-if="show">
+    ¡Clícame para revelar!
+  </button>
+  <p class="px-3 py-2 mt-4" v-else>
+    {{props.text}}
+  </p>
+</template>
