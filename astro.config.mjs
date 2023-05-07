fc4d4cf09cd7609d5e2b975b2a8cefa64af64344
NIU1599977 <1599977@uab.cat>
2023-05-07 23:47:45 +0200

    Ready to Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [tailwind(), vue()],
+  site: 'https://fir3l1ght.github.io',
+  base: '/astro-landingpage',
 });
--- src/components/landing/MysteryButton.vue
+++ src/components/landing/MysteryButton.vue
@@ -10,7 +10,7 @@ function hideButton() {
 </script>
 
 <template>
-  <button class="bg-orange-500 rounded-xl px-3 py-2 mt-4 hover:bg-indigo-400" @click="hideButton" v-if="show">
+  <button class="bg-orange-500 rounded-xl px-3 py-2 mt-4 hover:bg-zinc-300 hover:text-orange-600" @click="hideButton" v-if="show">
     ¡Clícame para revelar!
   </button>
   <p class="px-3 py-2 mt-4" v-else>
